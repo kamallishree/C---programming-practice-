af64b67671369d9e88eed3d2ae38712981e47b26
kamallishree <kamallishree911@gmail.com>
2026-02-13 20:12:17 +0530

    Create cube and square of a num.c

---
+++ cube and square of a num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,square=0,cube=0;
+    printf("enter a:");
+    scanf("%d",&a);
+    square=a*a;
+    cube=a*a*a;
+    printf("square = %d \n",square);
+    printf("cube = %d",cube);
+    return 0;
+}
