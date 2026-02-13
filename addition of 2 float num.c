acea7b91e096cef458478885d3a00c48775f3406
kamallishree <kamallishree911@gmail.com>
2026-02-13 20:17:26 +0530

    Create addition of 2 float num.c

---
+++ addition of 2 float num.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float a,b,sum=0;
+    printf("enter two float numbers: ");
+    scanf("%f%f",&a,&b);
+    sum=a+b;
+    printf("sum=%f \n",sum);
+    return 0;
+}
