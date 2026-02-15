f60a619777f3cbfb009d3003b28282f10e36b1a8
kamallishree <kamallishree911@gmail.com>
2026-02-15 20:16:48 +0530

    Create a program to assign grades based on marks.c

---
+++ a program to assign grades based on marks.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int marks;
+    printf("Enter your marks: ");
+    scanf("%d", &marks);
+    if (marks >= 90)
+        printf("Grade: A");
+    else if (marks >= 80)
+        printf("Grade: B");
+    else if (marks >= 70)
+        printf("Grade: C");
+    else if (marks >= 60)
+        printf("Grade: D");
+    else if (marks >= 50)
+        printf("Grade: E");
+    else
+        printf("Grade: F (Fail)");
+
+    return 0;
+}
