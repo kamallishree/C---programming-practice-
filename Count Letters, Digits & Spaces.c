7eb56f9464c08570c5d0bae0fc03ccb39b7a583a
kamallishree <kamallishree911@gmail.com>
2026-02-21 20:47:23 +0530

    Create Count Letters, Digits & Spaces.c

---
+++ Count Letters, Digits & Spaces.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[200];
+    int letters = 0, digits = 0, spaces = 0, i;
+
+    printf("Enter string:\n");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i]; i++) {
+        if(isalpha(str[i]))
+            letters++;
+        else if(isdigit(str[i]))
+            digits++;
+        else if(str[i] == ' ')
+            spaces++;
+    }
+
+    printf("Letters: %d\n", letters);
+    printf("Digits: %d\n", digits);
+    printf("Spaces: %d", spaces);
+
+    return 0;
+}
