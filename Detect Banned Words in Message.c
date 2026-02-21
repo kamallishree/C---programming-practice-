1b9d1f2ed11c49411c6fcd41c3db4cb4a392e158
kamallishree <kamallishree911@gmail.com>
2026-02-21 20:46:37 +0530

    Create Detect Banned Words in Message.c

---
+++ Detect Banned Words in Message.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char message[200];
+    char banned[20][50];
+    int n, i, count = 0;
+
+    printf("Enter user message:\n");
+    fgets(message, sizeof(message), stdin);
+
+    printf("Enter number of banned words:\n");
+    scanf("%d", &n);
+
+    printf("Enter banned words:\n");
+    for(i = 0; i < n; i++)
+        scanf("%s", banned[i]);
+
+    printf("Banned words detected:\n");
+
+    for(i = 0; i < n; i++) {
+        if(strstr(message, banned[i]) != NULL) {
+            printf("%s\n", banned[i]);
+            count++;
+        }
+    }
+
+    printf("Total banned words found: %d", count);
+
+    return 0;
+}
