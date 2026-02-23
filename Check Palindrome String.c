729fb448a9ff3cb26075e7b3ea435ed9b49c888d
kamallishree <kamallishree911@gmail.com>
2026-02-23 12:51:48 +0530

    Create Check Palindrome String.c

---
+++ Check Palindrome String.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, len = 0, flag = 1;
+
+    printf("Enter string: ");
+    scanf("%s", str);
+
+    while(str[len] != '\0')
+        len++;
+
+    for(i = 0; i < len/2; i++) {
+        if(str[i] != str[len-i-1]) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
