3e3272f3583785244f16d54154ad40a5e299c4a7
kamallishree <kamallishree911@gmail.com>
2026-02-23 12:45:29 +0530

    Create Concatenate two strings (strcat()).c

---
+++ Concatenate two strings (strcat()).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s1[100], s2[100];
+
+    printf("Enter first string: ");
+    scanf("%s", s1);
+
+    printf("Enter second string: ");
+    scanf("%s", s2);
+
+    strcat(s1, s2);
+
+    printf("Joined string: %s", s1);
+
+    return 0;
+}
