476f6e65c47a41afae87d0d7e9a8ff4332116fa3
kamallishree <kamallishree911@gmail.com>
2026-03-02 21:18:23 +0530

    Create Sort Array (Ascending Order).c

---
+++ Sort Array (Ascending Order).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int a[]={5,2,8,1,3};
+int i,j,temp,n=5;
+
+for(i=0;i<n-1;i++)
+ for(j=0;j<n-i-1;j++)
+  if(a[j]>a[j+1]){
+   temp=a[j];
+   a[j]=a[j+1];
+   a[j+1]=temp;
+  }
+
+for(i=0;i<n;i++)
+ printf("%d ",a[i]);
+}
